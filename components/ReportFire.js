0440fe43f7a2835b22b0d0439e734d60d3250648
mesterlum <mesterlum@hotmail.com>
2018-10-20 11:53:34 -0700

    Commit add camera options

--- components/ReportFire.js
+++ components/ReportFire.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 import {
-
+    View
 } from 'react-native'
 import {
-    Container
+    Container,
+    Textarea,
+    Button,
+    Text,
+    Icon
 } from 'native-base'
 import MapView, { Marker } from 'react-native-maps';
+import {showImagePicker} from 'react-native-image-picker';
+//TMP
+
+const options = {
+    title: 'Select Avatar',
+    customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+    storageOptions: {
+        skipBackup: true,
+        path: 'images',
+    },
+};
+
 
 export default class ReportFire extends Component {
     constructor(props) {
@@ -40,6 +56,12 @@ export default class ReportFire extends Component {
         this.getLocation()
     }
 
+    pressCamera() {
+        showImagePicker(options, (response) => {
+            console.log("response", response)
+        })
+    }
+
     render() {
         return (
             <Container>
@@ -52,8 +74,15 @@ export default class ReportFire extends Component {
                         onDragEnd={e => console.log(e.nativeEvent)}
                     ></MapView.Marker>
                 </MapView>
+                <Textarea style={{ marginTop: 10, marginHorizontal: 7 }} rowSpan={5} bordered placeholder="Descripcion" />
+                <View style={{ alignItems: 'center', marginTop: 15, marginHorizontal: 7, flexDirection: 'row', justifyContent: 'center' }}>
 
-
+                    <Icon name='camera' type="FontAwesome" style={{ marginRight: 10, color: '#4267b2' }} onPress={this.pressCamera}/>
+                    <Button iconLeft danger>
+                        <Icon name='fire' type="FontAwesome" />
+                        <Text>Enviar Reporte</Text>
+                    </Button>
+                </View>
             </Container>
         )
     }
