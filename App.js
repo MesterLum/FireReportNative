2266c73dc55be79f6fd6bfdabe30ca334a3851db
mesterlum <mesterlum@hotmail.com>
2018-10-19 23:18:24 -0700

    Implemented maps clean

--- App.js
+++ App.js
@@ -7,48 +7,50 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { AsyncStorage } from 'react-native'
 import MapView from 'react-native-maps'
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { Container, Text, Fab, Icon, Button } from 'native-base'
+import Modal from "react-native-modal"
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  componentWillMount() {
+    AsyncStorage.getItem("welcome1", (err, result) => {
+      if (err) {
+        console.log(err)
+      }
+      console.log(result)
+      if (result) {
+        console.log("Ya existe")
+      }
+      else {
+        AsyncStorage.setItem("welcome1", "true", err => {
+          if (err)
+            console.log(err)
+          else {
+            console.log("Se monto.")
+          }
+        })
+      }
+    })
+  }
+
   render() {
     return (
+      <Container>
+
         <MapView
-        style={styles.container}
+          style={{ flex: 1 }}
           initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+            latitude: 25.777766,
+            longitude: -108.991283,
+            latitudeDelta: 0.0100,
+            longitudeDelta: 0.050,
           }}
-        />
+        >
+        </MapView>
+      </Container >
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
