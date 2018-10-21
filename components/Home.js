1ab4f15f2b2789d1c729512a60c08bac2010c055
mesterlum <mesterlum@hotmail.com>
2018-10-20 19:08:22 -0600

    Implemented NASA APi

--- components/Home.js
+++ components/Home.js
@@ -4,9 +4,10 @@ import {
     Container,
     Fab,
     Icon,
-    Button
+    Button,
+    Text
 } from 'native-base'
-import MapView from 'react-native-maps'
+import MapView, { Marker, Callout } from 'react-native-maps'
 // Components
 import WelcomeHome from './Welcome'
 
@@ -17,8 +18,34 @@ export class Home extends Component {
         // Var for determinate welcome
         this.state = {
             welcome: false,
-            active: false
+            active: false,
+            fireDataWorl: undefined
         }
+
+        this.getDataFire = this.getDataFire.bind(this)
+    }
+
+    getDataFire() {
+        fetch('https://hawking.sv.cmu.edu:9016/opennex/getAPIResult', {
+            method: 'POST',
+            body: JSON.stringify({
+                "serviceId": 25,
+                "userId": 55,
+                "date1": "2015012",
+                "date2": "2018020",
+                "ulx": -120,
+                "uly": 25,
+                "lrx": -98,
+                "lry": 20,
+                "purpose": "Space Apps"
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(data => data.json())
+            .then(json => this.setState({ fireDataWorl: json }))
+            .catch(err => console.log(err))
     }
 
     checkWelcomeToken() {
@@ -36,39 +63,50 @@ export class Home extends Component {
 
     componentDidMount() {
         this.checkWelcomeToken()
+        this.getDataFire()
 
     }
 
     render() {
-        console.log(this.props.navigation)
+        console.log(this.state.fireDataWorl)
         return (
             <Container>
                 {this.state.welcome && <WelcomeHome />}
                 <MapView
                     style={{ flex: 1 }}
                     initialRegion={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
+                        latitude: 23.634501,
+                        longitude: -102.55278399999997,
+                        latitudeDelta: 0.100,
+                        longitudeDelta: 0.94
                     }}
                 >
+
+                    {
+                        this.state.fireDataWorl &&
+                        this.state.fireDataWorl.map((element, key) => {
+
+                            return (
+                                <Marker key={key} coordinate={{ latitude: element.lat, longitude: element.lon }}>
+                                    <Icon size={2} name="free-code-camp" type="FontAwesome" style={{ color: 'red' }} />
+                                    <Callout>
+                                        <Text>Hola a todos </Text>
+                                    </Callout>
+                                </Marker>
+                            )
+                        })
+                    }
 
                 </MapView>
 
 
-
-                
-
-
-                
-                <Fab    
+                <Fab
                     active={this.state.active}
                     containerStyle={{}}
                     style={{ backgroundColor: 'red' }}
                     position="bottomRight"
                     onPress={() => this.props.navigation.navigate('ReportFire')}>
-                    <Icon name="fire" type="FontAwesome"/>
+                    <Icon name="fire" type="FontAwesome" />
                 </Fab>
             </Container>
         )
